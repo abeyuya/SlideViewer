8dee0a9e64b4a59192f38550187aab4b35a3200e
abeyuya <yuya.abe.0525@gmail.com>
2017-12-26 00:32:18 +0900

    show indicator until setup thumbnail image

--- SlideViewer/Classes/ThumbnailTableViewCell.swift
+++ SlideViewer/Classes/ThumbnailTableViewCell.swift
@@ -11,12 +11,19 @@ import ReSwift
 final class ThumbnailTableViewCell: UITableViewCell {
     
     var index: Int? = nil
-    var thumbnail: UIImageView = {
+    var tableView: UITableView? = nil
+    
+    let thumbnail: UIImageView = {
         let v = UIImageView()
         v.translatesAutoresizingMaskIntoConstraints = false
         return v
     }()
-    var tableView: UITableView? = nil
+    let indicator: UIActivityIndicatorView = {
+        let v = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+        v.startAnimating()
+        v.translatesAutoresizingMaskIntoConstraints = false
+        return v
+    }()
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -33,7 +40,6 @@ final class ThumbnailTableViewCell: UITableViewCell {
     }
     
     override func prepareForReuse() {
-        // TODO: show loading indicator
         thumbnail.image = nil
     }
 
@@ -50,6 +56,7 @@ extension ThumbnailTableViewCell {
         
         if let image = mainStore.state.slide.thumbnailImages[index] {
             thumbnail.image = image
+            indicator.removeFromSuperview()
         } else {
             loadImage(state: mainStore.state, index: index)
         }
@@ -57,7 +64,6 @@ extension ThumbnailTableViewCell {
     
     private func setupImageView() {
         contentView.addSubview(thumbnail)
-        
         contentView.addConstraints([
             NSLayoutConstraint(
                 item: thumbnail,
@@ -92,6 +98,26 @@ extension ThumbnailTableViewCell {
                 multiplier: 1,
                 constant: 0),
             ])
+        
+        thumbnail.addSubview(indicator)
+        contentView.addConstraints([
+            NSLayoutConstraint(
+                item: indicator,
+                attribute: .centerX,
+                relatedBy: .equal,
+                toItem: thumbnail,
+                attribute: .centerX,
+                multiplier: 1,
+                constant: 0),
+            NSLayoutConstraint(
+                item: indicator,
+                attribute: .centerY,
+                relatedBy: .equal,
+                toItem: thumbnail,
+                attribute: .centerY,
+                multiplier: 1,
+                constant: 0),
+        ])
     }
 }
 
@@ -100,12 +126,13 @@ extension ThumbnailTableViewCell: StoreSubscriber {
     public typealias StoreSubscriberStateType = SlideViewerState
     
     public func newState(state: StoreSubscriberStateType) {
-        guard let index = index,
-            let tableView = tableView,
-            thumbnail.image == nil else { return }
+        guard thumbnail.image == nil,
+            let index = index,
+            let tableView = tableView else { return }
         
         if let image = state.slide.thumbnailImages[index] {
             thumbnail.image = image
+            indicator.removeFromSuperview()
             tableView.reloadData()
         }
     }
